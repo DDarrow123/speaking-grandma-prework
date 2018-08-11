22385c716efcf4d8c2b0fe62289235ac4996ad9b
Devon Darrow <ddarrowphl@gmail.com>
2018-08-11 22:19:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,10 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase == "phrase".upcase
    "NO, NOT SINCE 1938!"
-  end
   elsif phrase == "I LOVE YOU GRANDMA"
    "I LOVE YOU TOO PUMPKIN"
-  end
   else
   "HUH?! SPEAK UP, SONNY!"
   end
